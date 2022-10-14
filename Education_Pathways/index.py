cf8dcefb4ec4c53da8a0e7d0cd5d36a424fd5336
Joseph Sawaya <josephsawaya938@gmail.com>
2022-10-14 14:13:05 -0400

    change db

--- Education_Pathways/index.py
+++ Education_Pathways/index.py
@@ -11,8 +11,7 @@ app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 # MongoDB URI
-DB_URI = "mongodb+srv://Cansin:cv190499@a-star.roe6s.mongodb.net/A-Star?retryWrites=true&w=majority"
-app.config["MONGODB_HOST"] = DB_URI
+app.config["MONGODB_HOST"] = os.environ.get("MONGO_URI")
 
 config.init_app(app)
 config.init_db(app)
