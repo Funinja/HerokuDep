75e563211aee536e2b499e2a5b58e40d0cd54c1d
Joseph Sawaya <josephsawaya938@gmail.com>
2022-10-17 13:37:00 -0400

    Replace flask-mongoengine with pymongo
    This commit replaces flask-mongoengine with pymongo.
    This commit also introduces code to connect to the database
    and cache data on the server.

--- Education_Pathways/config.py
+++ Education_Pathways/config.py
@@ -1,11 +1,13 @@
 # This is to prevent circular imports
 
-from flask_mongoengine import MongoEngine
+from pymongo import MongoClient
+import os
 from flask_cors import CORS
 
 app = None
-db = None
+client = None
 cors = None
+list_of_course_strings = None
 
 def init_app(app_):
     global app
@@ -13,9 +15,15 @@ def init_app(app_):
     return app
 
 def init_db(app):
-    global db
-    db = MongoEngine(app)
-    return db
+    global client
+    global list_of_course_strings
+    pw = os.environ.get('MONGO_URI')
+    client = MongoClient(f'mongodb+srv://flask_app_user:{pw}@cluster0.dmqmrxd.mongodb.net/?retryWrites=true&w=majority')
+    db = client.courses
+    coll = db.get_collection('engineering')
+    list_of_courses = list(coll.find({}))
+    list_of_course_strings = [x['Course Code'] + ' ' + x['Course Name'] for x in list_of_courses]
+    return list_of_course_strings
 
 def init_cors(app):
     global cors
