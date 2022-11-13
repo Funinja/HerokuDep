e058c219a5939ca214061c48bb9157188b2fae06
Americo <americobarros8@gmail.com>
2022-11-13 00:42:28 -0500

    Add ToDo

--- Education_Pathways/frontend/src/components/AdminPanel.jsx
+++ Education_Pathways/frontend/src/components/AdminPanel.jsx
@@ -6,8 +6,18 @@ import Row from 'react-bootstrap/Row'
 
 export class AdminPanel extends Component {
   componentDidMount() {
-    
+
   }
+  /*
+  Todo:
+    - make controller that sends the syllabi collection
+    - make a function that populattes a row based on unput
+    - make a funcition that iterates throuhg all the courses to create the table
+    - make the table
+    - style the table
+    - pagination? - probably not necessary
+
+  */
   render() {
     return (
       <div>AdminPanel</div>
