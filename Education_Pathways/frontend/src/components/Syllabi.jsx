137215e56fc19f8bcb8191236754590d89bff0df
Americo <americobarros8@gmail.com>
2022-11-12 20:18:13 -0500

    Add more front end

--- Education_Pathways/frontend/src/components/Syllabi.jsx
+++ Education_Pathways/frontend/src/components/Syllabi.jsx
@@ -7,7 +7,7 @@ export class Syllabi extends Component {
     super(props);
     this.state = {
       course_code: props.course_code,
-      syllabus_link: undefined,
+      syllabus_link: "",
       request_count: 0,
       requested: false
     }
@@ -16,37 +16,66 @@ export class Syllabi extends Component {
   componentDidMount() {
     axios.get(`http://127.0.0.1:5000/course/syllabus`, {
       params:{
-        course: [this.props.course_code],
+        course_code: this.state.course_code
       },
       paramsSerializer: params => {
         return qs.stringify(params)
       }
     })
     .then(res => {
-      console.log(res.data)
-      if(res.data["link"]){
-        this.setState({syllabus_link: res.data["link"]});
-      }else if(res.data["request_count"]){
-        this.setState({request_count: res.data["request_count"]});
+      let course_syllabus_info = JSON.parse(res.data)[0]
+      if(course_syllabus_info.link){
+        this.setState({syllabus_link: course_syllabus_info.link});
+      }else if(course_syllabus_info.request_count){
+        this.setState({request_count: course_syllabus_info.request_count});
       }
     })
   }
   openLink() {
-    
+    const newWindow = window.open(this.state.syllabus_link, '_blacnk', 'noopener,noreferrer');
+    if (newWindow) {
+      newWindow.opener = null;
+    }
+  }
+  requestSyllabus = () => {
+    axios.post(`http://127.0.0.1:5000/course/syllabus`, {
+      params:{
+        course_code: this.state.course_code,
+      },
+      paramsSerializer: params => {
+        return qs.stringify(params)
+      }
+    }).then((res) => {
+      //Redo the getting or increase the count on the front end
+      this.setState({request_count: this.state.request_count + 1, requested: true});
+      console.log(res); 
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
   render() {
     if(this.state.syllabus_link){
       return (
         <button className={"syllabus-link"} onClick={this.openLink}>View</button>
       )
+    }else if (this.state.requested){
+      return (
+        <div className='syllabus-request'>
+          <button className={"syllabus-link"}>Already Requested</button>
+          <span className={"request-counter"} title="Number of requests made for this course">
+            {this.state.request_count}
+          </span>
+        </div>
+      )
     }else {
       return (
         <div className='syllabus-request'>
-            <button className={"syllabus-link"} onClick={this.openLink}>Request</button>
-            <span className={"request-counter"} title="Number of requests made for this course">
-              {this.state.requestCount}
-            </span>
-          </div>
+          <button className={"syllabus-link"} onClick={this.requestSyllabus}>Request</button>
+          <span className={"request-counter"} title="Number of requests made for this course">
+            {this.state.request_count}
+          </span>
+        </div>
       )
     }
   }
