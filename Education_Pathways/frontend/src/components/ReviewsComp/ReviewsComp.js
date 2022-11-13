d7544b6c7d82badbd3ac39fff1e50e93eeddc416
Michael Hong <michaelhong@Michaels-Mac-mini.local>
2022-11-12 21:38:24 -0500

    Removed Sample Data

--- Education_Pathways/frontend/src/components/ReviewsComp/ReviewsComp.js
+++ Education_Pathways/frontend/src/components/ReviewsComp/ReviewsComp.js
@@ -3,27 +3,6 @@ import ReviewsForm from "./ReviewsForm";
 import ReviewsList from "./ReviewsList";
 import styles from "./ReviewsComp.module.css";
 
-const SAMPLE_DATA = [
-  {
-    first: "Michael",
-    last: "Hong",
-    review: "I hate this course!",
-    rating: 4,
-  },
-  {
-    first: "David",
-    last: "Kim",
-    review: "I love this course!",
-    rating: 3,
-  },
-  {
-    first: "Jenny",
-    last: "Kim",
-    review: "I failed this course!",
-    rating: 2,
-  },
-];
-
 const ReviewsComp = (props) => {
   return (
     <div className={styles["reviews-comp"]}>
