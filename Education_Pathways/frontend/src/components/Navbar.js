a80640eb05552c28b2c30bc5ab3cd4014e0e01af
stahmasebi <sepehr.tahmasebi@student.unimelb.edu.au>
2022-11-15 03:44:25 -0500

    color of navbar changed

--- Education_Pathways/frontend/src/components/Navbar.js
+++ Education_Pathways/frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './css/navbar.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import logo from './img/logo.png'
+import logo from './img/uoft_logo.png'
 import { Navbar, Nav } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch, Link, useLocation } from "react-router-dom";
 // import LogIn from "./LogIn.jsx";
@@ -50,11 +50,11 @@ export default class NavbarComp extends Component {
     return (
       <Router>
         <div>
-          <Navbar bg="myBlue" variant="dark" sticky="top" expand="lg">
+          <Navbar bg="002a5c" variant="dark" sticky="top" expand="lg">
             <Navbar.Brand>
               <img src={logo} alt="" />{" "}
               <Nav.Link href="/" style={{ color: "white", display: "inline" }}>
-                Education Pathways
+                Coursify
               </Nav.Link>
             </Navbar.Brand>
 
@@ -62,7 +62,7 @@ export default class NavbarComp extends Component {
             <Navbar.Collapse>
               <Nav>
                 <Nav.Link as={Link} to="/about">
-                  About Us
+                  About
                 </Nav.Link>
 
                 <Nav.Link as={Link} to="/mylist">
@@ -73,8 +73,6 @@ export default class NavbarComp extends Component {
                   Search
                 </Nav.Link> */}
 
-
-
               </Nav>
             </Navbar.Collapse>
           </Navbar>
@@ -82,20 +80,21 @@ export default class NavbarComp extends Component {
         <div>
           <Switch>
             <Route path="/about">
-              <div className="body_text">
+              <div className="body_text" align="left">
                 <p>
 
-                  Welcome to CARTE's in-development tool for course selection at UofT. Education Pathways allows for more intelligent course searching, by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
+                  Welcome to IO's improvement of CARTE's in-development tool for course selection at UofT. Coursify is a modificaiton of CARTE's original Education Pathways platform that 
+                  
+                  allows for more intelligent course searching by matching not just the terms you search, but ones relevant to them. The more terms you search for, the more relevant your results will be! Even try searching across disciplines for the courses that best cover each.
 
                   Whatever year you are looking for, Education Pathways will also suggest courses in earlier years that will best help you to prepare. To get the most out of this, try searching for courses in a later year and see what is suggested for your current one.
 
-                  We are looking for feedback to improve Education Pathways and make it more useful for students. If you have ideas or suggestions, please <a href="mailto:alex.olson@utoronto.ca">  email us! </a> <br></br>
+                  We are looking for feedback to improve Education Pathways and make it more useful for students. 
                 </p>
                 <p>
                   <b>Development Team: </b>
                 </p>
-                <p>Alexander Olson <a href="https://carte.utoronto.ca/"> (CARTE)</a> </p>
-                <p>Student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2021</a> : Janelle Cuevas, Jean Lin, Terry Luan, Cansin Varol, Nick Woo</p>
+                <p>Alexander Olson <a href="https://carte.utoronto.ca/">(CARTE)</a> and student team from <a href="https://shuiblue.github.io/UofT-ECE444/">ECE444-Fall2022</a>: Americo B, Dennis L, Joseph S, Michael H and Sepehr T</p>
 
 
               </div>
